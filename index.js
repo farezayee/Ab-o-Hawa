20d4f0c80233b65f7d4455637690c720c0fdf3ff
farezayee <farezayee@yahoo.com>
2024-08-19 17:42:22 -0300

    updated css styles.

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-emoji"/>`;
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
@@ -35,30 +37,6 @@ function formatDate(date) {
 }
 function searchCity(city) {
   let apiKey = "3f82e744426ad147cf0oa3718d3b5f8t";
-
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-  axios.get(apiUrl).then(refreshWeather);
-}
-
-function handleSearchSubmit(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-form-input");
-
-  searchCity(searchInput.value);
-
-  let searchFormElement = document.querySelector("#search-form");
-  searchFormElement.addEventListener("submit", handleSearchSubmit);
-  searchCity("Lisbon");
-
-  let city = response.data.city;
-  console.log(response.data.temperature.humidity);
-
-  cityElement.innerHTML = city;
-  descriptionElement.innerHTML = response.data.condition.description;
-  temperatureElement.innerHTML = Math.round(temperature);
-}
-function searchCity(city) {
-  let apiKey = "3f82e744426ad147cf0oa3718d3b5f8t";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(refreshWeather);
 }
@@ -72,4 +50,4 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-searchCity("Lisbon");
+searchCity = "Lisbon";

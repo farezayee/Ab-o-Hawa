f281a0bb8776b6e59e6285efd6a4133f823dc16e
farezayee <farezayee@yahoo.com>
2024-08-20 09:18:44 -0300

    added the forecast element html and styled them in CSS.

--- index.js
+++ index.js
@@ -30,12 +30,14 @@ function formatDate(date) {
     "Friday",
     "Saturday",
   ];
-  let day = days[date.getDay()];
 
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
 
@@ -54,3 +56,4 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("Paris");
